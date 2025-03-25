22d03b291d85ce27c7f141bda8ea619673917bee
Salim Meshref <salimmeshref@Salims-MacBook-Pro.local>
2025-03-25 14:28:12 +0200

    login screeen

--- app/src/main/java/com/example/tedmobchallenge/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/tedmobchallenge/ui/login/LoginActivity.kt
@@ -1,12 +1,56 @@
 package com.example.tedmobchallenge.ui.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.tedmobchallenge.R
+import com.example.tedmobchallenge.databinding.ActivityLoginBinding
+import com.example.tedmobchallenge.ui.home.HomeActivity
+import data.PreferenceManager
 
 class LoginActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var preferencesManager: PreferenceManager
+
+
+    private fun setupUI() {
+        binding.btnLogin.setOnClickListener {
+            validateAndLogin()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        preferencesManager = PreferenceManager(this)
+
+        setupUI()
+    }
+
+    private fun validateAndLogin() {
+        val email = binding.email.text.toString().trim()
+        val password = binding.password.text.toString().trim()
+
+        if (email.isEmpty()) {
+            binding.inputEmail.error = getString(R.string.required)
+            return
+        }
+
+        if (password.isEmpty()) {
+            binding.inputPassword.error = getString(R.string.required)
+            return
+        }
+
+        preferencesManager.saveUserCredentials(email, password)
+
+        preferencesManager.saveUserProfile("TED", "MOB")
+
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
